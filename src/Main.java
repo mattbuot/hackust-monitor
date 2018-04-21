04f0713fac8202f28de3c94a5b6f95146f585b50
mattbuot <matthieu2301@hotmail.fr>
2018-04-21 18:01:04 +0800

    Basic Reader

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    private static final String NAVIGATION_HISTORY = "";
+
+    public static void main(String[] args) throws IOException {
+
         System.out.println("Hello World!");
+
+        String inputPath = NAVIGATION_HISTORY;
+
+        String line;
+        BufferedReader reader = new BufferedReader(new FileReader(inputPath));
+        try {
+            line = reader.readLine();
+            while (line != null) {
+                loadLine(String line);
+            }
+        } finally {
+            reader.close();
+        }
+
+    }
+
+    private static void loadLine(String line) {
+
     }
 }
