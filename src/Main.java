7a87f48f4adc9bcc43f80a602f3ddc7a482a1ca9
jheitmann <julien.heitmann@epfl.ch>
2018-04-21 18:58:19 +0800

    Reading history

--- src/Main.java
+++ src/Main.java
@@ -2,36 +2,66 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import History.Entry;
 
 public class Main {
 
 	// ask the user to enter location
-    private static final String NAVIGATION_HISTORY = "/home/julien/.config/google-chrome/Default/History";
+    private static final String NAVIGATION_HISTORY = "jdbc:sqlite:/home/julien/.config/google-chrome/Default/History";
+    private static Connection conn = null;
     
     public static void connect(String path) {
-        Connection conn = null;
         try {
             // db parameters
             // create a connection to the database
-            conn = DriverManager.getConnection(path);
+            conn = DriverManager.getConnection(NAVIGATION_HISTORY);
             
             System.out.println("Connection to SQLite has been established.");
             
         } catch (SQLException e) {
             System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
+        } 
+    }
+    
+    public static void close() {
+        try {
+            if (conn != null) {
+                conn.close();
             }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+    public static ArrayList<Entry> fetchHistory(){
+        String sql = "SELECT * FROM urls";
+        ArrayList<Entry> entries = new ArrayList<>();
+        
+        try (Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+            
+            // loop through the result set
+            while (rs.next()) {
+            	Entry nextEntry = new Entry(rs.getString("url"),rs.getString("title"),rs.getInt("visit_count"));
+            	entries.add(nextEntry);
+            }
+            return entries;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return null;
         }
     }
 
     public static void main(String[] args) throws IOException {
     	connect(NAVIGATION_HISTORY);
-    	
+    	ArrayList<Entry> entries = fetchHistory();
+    	for(Entry nextEntry: entries) {
+    		System.out.println(nextEntry);
+    	}
+    	close();
     }
 }
